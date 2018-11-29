bd49d8a1704152f3593c87359692642c72b22aa8
Matt Mitchell <mattmitchell650@gmail.com>
2018-11-29 11:11:19 -0800

    checkout items, tie charges to customer id

--- controllers/profile.js
+++ controllers/profile.js
@@ -12,14 +12,28 @@ const Item = require('../models/items');
  * Fetch app user token + info
  */
 router.get('/', async function (req, res) {
-  let stripeCustomerId = req.user.stripeCustomerId
+  const stripeCustomerId = req.user.stripeCustomerId
+  const purchase = {
+    total: req.query.amount,
+    id: req.query.tid,
+    description: req.query.description
+  }
 
   // fetch all available items
   const items = await Item.find()
 
+  // fetch all previous customer purchases, limit 20
+  const previousCharges = await stripe.charges.list({
+    limit: 20,
+    customer: stripeCustomerId
+  });
+  console.log(previousCharges);
+
   res.render('pages/profile', {
     user: req.user,
     items: items,
+    charges: previousCharges.data,
+    purchase: purchase.total ? purchase : null,
     stripeKey: process.env.STRIPE_PUBLISHABLE_KEY
   });
 });
@@ -27,18 +41,46 @@ router.get('/', async function (req, res) {
 
 router.post('/charge', async function(req, res) {
   const token = req.body.stripeToken; // Using Express
-  console.log(token);
+  const itemIds = req.body.itemIds;
+  const stripeCustomerId = req.user.stripeCustomerId
+  let currentItem;
+  let totalCost = 0;
+  let description = "";
 
-  const charge = await stripe.charges.create({
-    amount: 999,
-    currency: 'usd',
-    description: 'Example charge',
-    source: token,
+  // tally up total cost and add item names to description
+  // determine if there is 1 item or multiple items
+  if(itemIds instanceof Array) {
+    for(let i = 0; i < itemIds.length; i++) {
+      currentItem = await Item.findById(itemIds[i]);
+      totalCost += currentItem.price;
+
+      if(description.length == 0) {
+        description = currentItem.name
+      } else {
+        description += ", " + currentItem.name;
+      }
+    }
+  } else {
+    currentItem = await Item.findById(itemIds);
+    description = currentItem.name;
+    totalCost += currentItem.price;
+  }
+
+  // link card source with customer
+  const customer = await stripe.customers.update(stripeCustomerId, {
+    source: token
   });
 
-  console.log(charge);
+  // create charge
+  const charge = await stripe.charges.create({
+    amount: totalCost,
+    currency: 'usd',
+    description: description,
+    source: customer.default_source,
+    customer: stripeCustomerId
+  });
 
-  res.redirect('/')
+  res.redirect('/profile?amount=' + charge.amount + '&tid=' + charge.id + "&description=" + charge.description)
 });
 
 
