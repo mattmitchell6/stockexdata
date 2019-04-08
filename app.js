e97a196b135b8f68ef9c3f51d9a55a4794f0f290
Matthew Mitchell <mattmitchell@stripe.com>
2019-04-08 08:59:15 -0700

    reformatting tabs

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var app = express();
 // view engine setup (Handlebars)
 app.set('view engine', 'hbs');
 app.engine('hbs', exphbs({
-	extname: '.hbs',
-	helpers: handlebarsHelpers
+  extname: '.hbs',
+  helpers: handlebarsHelpers
 }));
 app.set('views','./views');
 
@@ -43,18 +43,18 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 // include recurring session variables to route
 app.use(function(req, res, next) {
-	// initialize configuration obj if empty
-	if(!req.session.config) {
-		req.session.config = {
-			checkout: "custom",
-			customLogo: null
-		}
-	}
-	res.locals.user = req.user;
-	res.locals.success_message = req.flash('success');
-	res.locals.error_message = req.flash('error');
-	res.locals.stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
-	res.locals.config = req.session.config
+  // initialize configuration obj if empty
+  if(!req.session.config) {
+    req.session.config = {
+      checkout: "custom",
+      customLogo: null
+    }
+  }
+  res.locals.user = req.user;
+  res.locals.success_message = req.flash('success');
+  res.locals.error_message = req.flash('error');
+  res.locals.stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+  res.locals.config = req.session.config
   next();
 });
 
@@ -63,17 +63,17 @@ app.use(require('./controllers'));
 
 // error handling
 app.use((err, req, res, next) => {
-	console.log(err);
-	res.render('pages/error', { error: err});
+  console.log(err);
+  res.render('pages/error', { error: err});
 });
 
 
 //start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log('Getting served on port ' + PORT);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Getting served on port ' + PORT);
+  }
 });
